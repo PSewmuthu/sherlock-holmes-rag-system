86d9ad6fc6f9c0c60f72381f699d3dc6f736532b
Pasindu Sewmuthu <apsewmuthu@gmail.com>
2026-03-31 16:37:58 +0530

    Create llm module for load llm, and embedding model

---
+++ llm.py
@@ -0,0 +1,23 @@
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+
+
+def load_llm():
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-2.5-flash",
+        temperature=0
+    )
+
+    return llm
+
+
+def load_embeddings():
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="gemini-embedding-2-preview")
+
+    return embeddings
